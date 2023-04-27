644249d6014c7b4836be645b40024b4ca2798c74
ivansp <ivan.sp100@gmail.com>
2023-04-27 17:16:35 -0400

    finalizacion de rebound_s3

---
+++ sesion3/rebound_s3.py
@@ -0,0 +1,16 @@
+"""Requerimos imprimir en pantalla el resultado de la multiplicación de dos variables, las cuales
+llamaremos “a y b”, y les asignaremos los valores 20 y 30, respectivamente. Debemos documentar
+cada paso de la operación haciendo uso de comentarios. """
+
+# creamos la variable a
+a = 20
+
+# creamos la variable b
+b = 30
+
+# creamos la variable para la multiplicacion
+multiplicacion = a*b
+
+# se imprime la variable multiplicacion con un print
+print(
+    f"El resultado de la multiplicacion entre {a} y {b} es: {multiplicacion} ")
