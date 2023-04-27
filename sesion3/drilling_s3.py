2947394ed5b2366b3370be6eedd0c9539a5c9a51
ivansp <ivan.sp100@gmail.com>
2023-04-27 17:52:27 -0400

    finalizacion del drilling_s3

--- sesion3/drilling_s3.py
+++ sesion3/drilling_s3.py
@@ -1,3 +1,16 @@
-"""Requerimos imprimir en pantalla el resultado de la multiplicación de dos variables, las cuales
-llamaremos “a y b”, y les asignaremos los valores 20 y 30, respectivamente. Debemos documentar
-cada paso de la operación haciendo uso de comentarios. """
+"""Requerimos hacer el cálculo de la raíz cuadrada de una variable llamada “y” que tiene valor 81,
+utilizando el módulo de la librería math incorporada en Python. El resultado debemos asignarlo a
+una variable que será impresa en pantalla. """
+
+
+# importamos la libreria math
+import math
+
+# creamos variable y
+y = 81
+
+# creamos la variable resultado
+resultado = math.sqrt(y)
+
+# imprimimos el resultado
+print(f"La raiz cuadrada de {y} es: {resultado} ")
