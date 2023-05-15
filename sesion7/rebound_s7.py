bab1a68ff02d7953e936cb18cd13f9ffe99891ff
ivansp <ivan.sp100@gmail.com>
2023-05-15 12:12:30 -0400

    Inclui en el codigo la condicion para determinar si el numero es un cero

--- sesion7/rebound_s7.py
+++ sesion7/rebound_s7.py
@@ -3,11 +3,14 @@
 
 
 # creamos la lista de numeros enteros
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista = [-3, -2, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# recorrimos la lista y aplicamos condicionales para determinar si es par o impar
+# recorrimos la lista y aplicamos condicionales para determinar si es par,impar o cero
 for num in lista:
-    if (num % 2 == 0):
+
+    if (num == 0):
+        print(f"El número es {num}")
+    elif (num % 2 == 0):
         print(f"El número {num} es un número par")
-    else:
+    elif (num % 2 != 0):
         print(f"El número {num} es un número impar")
